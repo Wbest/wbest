9034962636906817a7557e347818686b41bc6035
happyYOgurt <tmishel@yandex.ru>
2016-01-14 17:27:14 +0300

    добавил блог
    блог и статьи.  Для перехода к статье в блоге-карусели толи 2, толи 3 статья.

---
+++ blog/articles/index.php
@@ -0,0 +1,164 @@
+<?
+	header("Cache-Control: public");
+	header("Expires: " . date("r", time() + 3600));
+	$title = "Разработка корпоративных сайтов в сферах производства, медицины, недвижимости";
+	$description = "";
+	$keywords = "";
+	$meta = '';
+	$scriptOrg = '<script type="application/ld+json"></script>';
+	// include $_SERVER['DOCUMENT_ROOT']."/pn/bd.php";
+	include $_SERVER['DOCUMENT_ROOT']."/data/header.html";
+?>
+
+	<section class="page-blog-articles">
+	  	<div class="wrapper-header">
+		  		<header>
+					<h1>
+						15 вещей, которые нужно учесть перед запуском нового сайта
+					</h1>
+				</header>
+				<footer class="article-date">
+					21.123.123
+				</footer>
+			<div style="clear:both"></div>
+		</div>		
+
+			<div class="underline"></div>
+		
+		<div class="wrapper">
+				<div class="wrapper-blog-article">
+					<article>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus voluptatem deleniti, iusto porro quasi aut est incidunt eum. Aliquid earum, culpa dolores aspernatur tempore velit beatae porro totam. Et saepe, illum doloribus pariatur eum, dolores nemo ut eos assumenda mollitia tenetur atque recusandae dignissimos optio molestiae possimus adipisci, minima aliquam perferendis. Delectus molestiae odit quas porro incidunt aspernatur quasi voluptatum consequatur, dolorem rem temporibus fugit, necessitatibus saepe voluptatibus, eligendi ad. Quae eaque, error voluptatem doloremque tenetur accusamus fuga dolore ipsam non sit at veniam perferendis quia dignissimos aut consectetur praesentium illo assumenda molestias cupiditate aliquid nihil similique unde omnis. Quos.</p>
+
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus voluptatem deleniti, iusto porro quasi aut est incidunt eum. Aliquid earum, culpa dolores aspernatur tempore velit beatae porro totam. Et saepe, illum doloribus pariatur eum, dolores nemo ut eos assumenda mollitia tenetur atque recusandae dignissimos optio molestiae possimus adipisci, minima aliquam perferendis. Delectus molestiae odit quas porro incidunt aspernatur quasi voluptatum consequatur, dolorem rem temporibus fugit, necessitatibus saepe voluptatibus, eligendi ad. Quae eaque, error voluptatem doloremque tenetur accusamus fuga dolore ipsam non sit at veniam perferendis quia dignissimos aut consectetur praesentium illo assumenda molestias cupiditate aliquid nihil similique unde omnis. Quos.</p>
+						
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus voluptatem deleniti, iusto porro quasi aut est incidunt eum. Aliquid earum, culpa dolores aspernatur tempore velit beatae porro totam. Et saepe, illum doloribus pariatur eum, dolores nemo ut eos assumenda mollitia tenetur atque recusandae dignissimos optio molestiae possimus adipisci, minima aliquam perferendis. Delectus molestiae odit quas porro incidunt aspernatur quasi voluptatum consequatur, dolorem rem temporibus fugit, necessitatibus saepe voluptatibus, eligendi ad. Quae eaque, error voluptatem doloremque tenetur accusamus fuga dolore ipsam non sit at veniam perferendis quia dignissimos aut consectetur praesentium illo assumenda molestias cupiditate aliquid nihil similique unde omnis. Quos.</p>
+					</article>
+				</div>
+
+				<aside class="blog-aside-block">
+					
+					<section class="block-buttons-social">
+						
+						<div class="blocks text-share">
+							<span>Поделиться</span>
+						</div>
+
+							<div class="blocks">
+								<div class="aside-social-button">
+									<span class="icon-7"></span>
+								</div>
+								<div class="aside-social-button">
+									<span class="icon-6"></span>
+								</div>						
+								<div class="aside-social-button">
+									<span class="icon-5"></span>
+								</div>
+								<div class="aside-social-button">
+									<span class="icon-4"></span>
+								</div>
+							</div>
+
+					</section>
+
+					<section class="block-tag-buttons">
+						<a href="#">
+							<div class="tag-button">
+								<span>Дизайн</span>
+							</div>
+						</a>
+						<a href="#">
+							<div class="tag-button">
+								<span>Дизайн</span>
+							</div>
+						</a>
+						<a href="#">
+							<div class="tag-button">
+								<span>Продвижение</span>
+							</div>
+						</a>
+						<a href="#">
+							<div class="tag-button">
+								<span>Дизайн</span>
+							</div>
+						</a>
+						<a href="#">
+							<div class="tag-button">
+								<span>Продвижение</span>
+							</div>
+						</a>
+						<a href="#">
+							<div class="tag-button">
+								<span>Продвижение</span>
+							</div>
+						</a>
+						<a href="#">
+							<div class="tag-button">
+								<span>Дизайн</span>
+							</div>
+						</a>
+					<div style="clear:both"></div>	
+					</section>
+					<section class="block-list-articles">
+						<div class="block-article">
+							<header>
+								<h2>
+									15 вещей, которые нужно учесть перед запуском нового сайта	
+								</h2>
+							</header>
+							<footer>
+								<span class="block-article-date">
+									123.123.123
+								</span>
+									<a href="">Читать</a>
+							</footer>
+						</div>
+						<div class="block-article">
+							<header>
+								<h2>
+									15 вещей, которые нужно учесть перед запуском нового сайта	
+								</h2>
+							</header>
+							<footer>
+								<span class="block-article-date">
+									123.123.123
+								</span>
+									<a href="">Читать</a>
+							</footer>
+						</div>
+						<div class="block-article">
+							<header>
+								<h2>
+									15 вещей, которые нужно учесть перед запуском нового сайта	
+								</h2>
+							</header>
+							<footer>
+								<span class="block-article-date">
+									123.123.123
+								</span>
+									<a href="">Читать</a>
+							</footer>
+						</div>
+						<div class="block-article">
+							<header>
+								<h2>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio assumenda totam nisi omnis tenetur consectetur voluptate corrupti laboriosam quo ab porro, consequatur doloribus id minus.	
+								</h2>
+							</header>
+							<footer>
+								<span class="block-article-date">
+									123.123.123
+								</span>
+									<a href="">Читать</a>
+							</footer>
+						</div>
+					</section>
+				</aside>
+			<div style="clear:both"></div>
+		</div>
+	</section>
+
+
+<?
+	include $_SERVER['DOCUMENT_ROOT']."/data/footer.html";
+?>
--- blog/index.php
+++ blog/index.php
@@ -11,12 +11,281 @@
 
 		<!-- Content начало -->
 		
+		<main class="page-blog fon-page">
+			<div class="page-mask"></div>
+
+		<div class="header-mask"></div>
+		<div class="footer-mask"></div>
+
+		<div class="module module--vertical">
+			<div id="Carousel" class="glide">
+
+				<div class="glide__wrapper">
+					<ul class="glide__track">
+						<li class="glide__slide">
+							<div class="box">
+
+								<article class="box-content">
+									<header>
+										<h2>15 вещей, которые нужно учесть перед запуском нового сайта</h2>
+									</header>
+										<p>Ежедневно в Сети появляются тысячи новых сайтов, большинство из которых являются незаконченными, поскольку их владельцы действительно не знают о недоделках либо же просто забывают/ленятся доработать свои проекты. Это проблема, последствия которой могут негативно повлиять на доверие пользователей и авторитет сайта.</p>
+									<footer class="projects-more">
+										<div class="article-date">дата</div>
+										<a href="#" class="article-more">
+											<svg class="arrow">
+											<!-- Первая слева точка -->
+											<circle class="knot arrow-knot" r="3" cx="15" cy="30" data-svg-origin="12 28"></circle>
+											<!-- Основная линия -->
+											<line class="line arrow-line" x1="15" y1="30" x2="50" y2="30" data-svg-origin="60 30" style="stroke-width: 2px;transform-origin: 0px 0px 0px;"></line>
+											<!-- Правая точка в конце линии -->
+											<circle class="knot arrow-knot" r="3" cx="50" cy="30" data-svg-origin="72 28"></circle>
+											<!-- Верхняя точка -->
+											<circle class="knot arrow-knot-2" r="3" cx="40" cy="17" data-svg-origin="30 9"></circle>
+											<!-- Нижняя точка -->
+											<circle class="knot arrow-knot-3" r="3" cx="40" cy="43" data-svg-origin="30 45"></circle>
+											<!-- Верхняя линия -->
+											<line class="line arrow-line-2" x1="50" y1="28" x2="47" y2="17" data-svg-origin="15 30"></line>
+											<!-- Нижняя линия -->
+											<line class="line arrow-line-3" x1="50" y1="30" x2="50" y2="43" data-svg-origin="30 30"></line>
+											</svg>
+										Читать подробнее</a>
+									</footer>
+								</article>
+
+							</div>
+						</li>
+						<li class="glide__slide">
+							<div class="box">
+								
+								<article class="box-content">
+									<header>
+										<h2>15 вещей, которые нужно учесть перед запуском нового сайта</h2>
+									</header>
+										<p>Ежедневно в Сети появляются тысячи новых сайтов, большинство из которых являются незаконченными, поскольку их владельцы действительно не знают о недоделках либо же просто забывают/ленятся доработать свои проекты. Это проблема, последствия которой могут негативно повлиять на доверие пользователей и авторитет сайта.</p>
+									<footer class="projects-more">
+										<div class="article-date">дата</div>
+										<a href="articles/index.php" class="article-more">
+											<svg class="arrow">
+											<!-- Первая слева точка -->
+											<circle class="knot arrow-knot" r="3" cx="15" cy="30" data-svg-origin="12 28"></circle>
+											<!-- Основная линия -->
+											<line class="line arrow-line" x1="15" y1="30" x2="50" y2="30" data-svg-origin="60 30" style="stroke-width: 2px;transform-origin: 0px 0px 0px;"></line>
+											<!-- Правая точка в конце линии -->
+											<circle class="knot arrow-knot" r="3" cx="50" cy="30" data-svg-origin="72 28"></circle>
+											<!-- Верхняя точка -->
+											<circle class="knot arrow-knot-2" r="3" cx="40" cy="17" data-svg-origin="30 9"></circle>
+											<!-- Нижняя точка -->
+											<circle class="knot arrow-knot-3" r="3" cx="40" cy="43" data-svg-origin="30 45"></circle>
+											<!-- Верхняя линия -->
+											<line class="line arrow-line-2" x1="50" y1="28" x2="47" y2="17" data-svg-origin="15 30"></line>
+											<!-- Нижняя линия -->
+											<line class="line arrow-line-3" x1="50" y1="30" x2="50" y2="43" data-svg-origin="30 30"></line>
+											</svg>
+										Читать подробнее</a>
+									</footer>
+								</article>
+
+							</div>
+						</li>
+
+						<li class="glide__slide">
+							<div class="box">
+
+								<article class="box-content">
+									<header>
+										<h2>15 вещей, которые нужно учесть перед запуском нового сайта</h2>
+									</header>
+										<p>Ежедневно в Сети появляются тысячи новых сайтов, большинство из которых являются незаконченными, поскольку их владельцы действительно не знают о недоделках либо же просто забывают/ленятся доработать свои проекты. Это проблема, последствия которой могут негативно повлиять на доверие пользователей и авторитет сайта.</p>
+									<footer class="projects-more">
+										<div class="article-date">дата</div>
+										<a href="#" class="article-more">
+											<svg class="arrow">
+											<!-- Первая слева точка -->
+											<circle class="knot arrow-knot" r="3" cx="15" cy="30" data-svg-origin="12 28"></circle>
+											<!-- Основная линия -->
+											<line class="line arrow-line" x1="15" y1="30" x2="50" y2="30" data-svg-origin="60 30" style="stroke-width: 2px;transform-origin: 0px 0px 0px;"></line>
+											<!-- Правая точка в конце линии -->
+											<circle class="knot arrow-knot" r="3" cx="50" cy="30" data-svg-origin="72 28"></circle>
+											<!-- Верхняя точка -->
+											<circle class="knot arrow-knot-2" r="3" cx="40" cy="17" data-svg-origin="30 9"></circle>
+											<!-- Нижняя точка -->
+											<circle class="knot arrow-knot-3" r="3" cx="40" cy="43" data-svg-origin="30 45"></circle>
+											<!-- Верхняя линия -->
+											<line class="line arrow-line-2" x1="50" y1="28" x2="47" y2="17" data-svg-origin="15 30"></line>
+											<!-- Нижняя линия -->
+											<line class="line arrow-line-3" x1="50" y1="30" x2="50" y2="43" data-svg-origin="30 30"></line>
+											</svg>
+										Читать подробнее</a>
+									</footer>
+								</article>
+									
+							</div>
+						</li>
+
+						<li class="glide__slide">
+							<div class="box">
+
+								<article class="box-content">
+									<header>
+										<h2>15 вещей, которые нужно учесть перед запуском нового сайта</h2>
+									</header>
+										<p>Ежедневно в Сети появляются тысячи новых сайтов, большинство из которых являются незаконченными, поскольку их владельцы действительно не знают о недоделках либо же просто забывают/ленятся доработать свои проекты. Это проблема, последствия которой могут негативно повлиять на доверие пользователей и авторитет сайта.</p>
+									<footer class="projects-more">
+										<div class="article-date">дата</div>
+										<a href="#" class="article-more">
+											<svg class="arrow">
+											<!-- Первая слева точка -->
+											<circle class="knot arrow-knot" r="3" cx="15" cy="30" data-svg-origin="12 28"></circle>
+											<!-- Основная линия -->
+											<line class="line arrow-line" x1="15" y1="30" x2="50" y2="30" data-svg-origin="60 30" style="stroke-width: 2px;transform-origin: 0px 0px 0px;"></line>
+											<!-- Правая точка в конце линии -->
+											<circle class="knot arrow-knot" r="3" cx="50" cy="30" data-svg-origin="72 28"></circle>
+											<!-- Верхняя точка -->
+											<circle class="knot arrow-knot-2" r="3" cx="40" cy="17" data-svg-origin="30 9"></circle>
+											<!-- Нижняя точка -->
+											<circle class="knot arrow-knot-3" r="3" cx="40" cy="43" data-svg-origin="30 45"></circle>
+											<!-- Верхняя линия -->
+											<line class="line arrow-line-2" x1="50" y1="28" x2="47" y2="17" data-svg-origin="15 30"></line>
+											<!-- Нижняя линия -->
+											<line class="line arrow-line-3" x1="50" y1="30" x2="50" y2="43" data-svg-origin="30 30"></line>
+											</svg>
+										Читать подробнее</a>
+									</footer>
+								</article>
+									
+							</div>
+						</li>
+
+						<li class="glide__slide">
+							<div class="box">
+								
+								<article class="box-content">
+									<header>
+										<h2>15 вещей, которые нужно учесть перед запуском нового сайта</h2>
+									</header>
+										<p>Ежедневно в Сети появляются тысячи новых сайтов, большинство из которых являются незаконченными, поскольку их владельцы действительно не знают о недоделках либо же просто забывают/ленятся доработать свои проекты. Это проблема, последствия которой могут негативно повлиять на доверие пользователей и авторитет сайта.</p>
+									<footer class="projects-more">
+										<div class="article-date">дата</div>
+										<a href="#" class="article-more">
+											<svg class="arrow">
+											<!-- Первая слева точка -->
+											<circle class="knot arrow-knot" r="3" cx="15" cy="30" data-svg-origin="12 28"></circle>
+											<!-- Основная линия -->
+											<line class="line arrow-line" x1="15" y1="30" x2="50" y2="30" data-svg-origin="60 30" style="stroke-width: 2px;transform-origin: 0px 0px 0px;"></line>
+											<!-- Правая точка в конце линии -->
+											<circle class="knot arrow-knot" r="3" cx="50" cy="30" data-svg-origin="72 28"></circle>
+											<!-- Верхняя точка -->
+											<circle class="knot arrow-knot-2" r="3" cx="40" cy="17" data-svg-origin="30 9"></circle>
+											<!-- Нижняя точка -->
+											<circle class="knot arrow-knot-3" r="3" cx="40" cy="43" data-svg-origin="30 45"></circle>
+											<!-- Верхняя линия -->
+											<line class="line arrow-line-2" x1="50" y1="28" x2="47" y2="17" data-svg-origin="15 30"></line>
+											<!-- Нижняя линия -->
+											<line class="line arrow-line-3" x1="50" y1="30" x2="50" y2="43" data-svg-origin="30 30"></line>
+											</svg>
+										Читать подробнее</a>
+									</footer>
+								</article>
+									
+							</div>
+						</li>
+						<li class="glide__slide">
+							<div class="box">
+								
+								<article class="box-content">
+									<header>
+										<h2>15 вещей, которые нужно учесть перед запуском нового сайта</h2>
+									</header>
+										<p>Ежедневно в Сети появляются тысячи новых сайтов, большинство из которых являются незаконченными, поскольку их владельцы действительно не знают о недоделках либо же просто забывают/ленятся доработать свои проекты. Это проблема, последствия которой могут негативно повлиять на доверие пользователей и авторитет сайта.</p>
+									<footer class="projects-more">
+										<div class="article-date">дата</div>
+										<a href="#" class="article-more">
+											<svg class="arrow">
+											<!-- Первая слева точка -->
+											<circle class="knot arrow-knot" r="3" cx="15" cy="30" data-svg-origin="12 28"></circle>
+											<!-- Основная линия -->
+											<line class="line arrow-line" x1="15" y1="30" x2="50" y2="30" data-svg-origin="60 30" style="stroke-width: 2px;transform-origin: 0px 0px 0px;"></line>
+											<!-- Правая точка в конце линии -->
+											<circle class="knot arrow-knot" r="3" cx="50" cy="30" data-svg-origin="72 28"></circle>
+											<!-- Верхняя точка -->
+											<circle class="knot arrow-knot-2" r="3" cx="40" cy="17" data-svg-origin="30 9"></circle>
+											<!-- Нижняя точка -->
+											<circle class="knot arrow-knot-3" r="3" cx="40" cy="43" data-svg-origin="30 45"></circle>
+											<!-- Верхняя линия -->
+											<line class="line arrow-line-2" x1="50" y1="28" x2="47" y2="17" data-svg-origin="15 30"></line>
+											<!-- Нижняя линия -->
+											<line class="line arrow-line-3" x1="50" y1="30" x2="50" y2="43" data-svg-origin="30 30"></line>
+											</svg>
+										Читать подробнее</a>
+									</footer>
+								</article>
+									
+							</div>
+						</li>												
+					</ul>
+				</div>
+
+			</div>
+		</div>
+			
+
+		</main>
+
 		</div>	
 		<!-- Content конец -->
 		<!-- Подключенные скрипты -->
-
+		
         <script src="../dist/js/jquery.min.js"></script>
-		<script src="../dist/js/all.js"></script>		
+		<script src="../dist/js/all.js"></script>
+		<script src="../dist/js/glide.min.js"></script>
+		<script>
+
+			var scl,paddingSize; // Create a variable
+			var heightScreen = $(document).height();
+
+				window.setInterval(function(){
+				   scl=0; // Reset this variable every 1.5 seconds
+				}, 700);
+
+
+			if((heightScreen>=705) && (heightScreen<850)){
+				paddingSize='16%';
+				$('.header-mask').css('height','18%');
+				$('.footer-mask').css('height','14%');
+			}else if((heightScreen>=850) && (heightScreen<1150)){
+				paddingSize='23%';
+				$('.header-mask').css('height','25%');
+				$('.footer-mask').css('height','21%');
+			}
+
+			var carousel = $('#Carousel').glide({
+				type: 'carousel',
+				mode: 'vertical',
+				animationTimingFunc:'cubic-bezier(0.165, 0.240, 0.440, 0.440)',
+				paddings: paddingSize,
+				keyboard: true,
+				autoplay: false,
+			});
+
+			$(window).on('DOMMouseScroll mousewheel', function(event) {
+				var glide_api = $(".glide").data('glide_api');
+			    if (event.originalEvent.wheelDelta > 0) {
+			    	if (scl===0){
+			    		// Вверх 
+			        	glide_api.go('<');
+			        	scl=10;
+			        }
+			    }
+			    else {
+			    	if (scl==0){
+			    		// Вниз
+			        	glide_api.go('>');
+			        	scl=10;
+			        }	
+			    }
+			}	);
+		</script>		
 		
 </body>
 </html>
